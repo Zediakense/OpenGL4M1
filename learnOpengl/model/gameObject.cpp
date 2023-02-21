75c3dfd1f6c42ca8a0cef9e3348d57ef317c5e28
MoChuan <zuosicheng@xiaohongshu.com>
2023-02-21 14:15:28 +0800

    fix：wrap of gameobject

--- learnOpengl/model/gameObject.cpp
+++ learnOpengl/model/gameObject.cpp
@@ -2,7 +2,34 @@
 //  gameObject.cpp
 //  learnOpengl
 //
-//  Created by user on 2023/2/17.
+//  Created by Suchantso on 2023/2/17.
 //
 
 #include "gameObject.hpp"
+
+
+GameObject::GameObject(std::string ModelPath,std::string shaderPath):
+m_modelPath(ModelPath),
+m_shaderPath(shaderPath)
+{
+    m_shader = std::make_shared<Shader>(m_shaderPath);
+    m_model  = std::make_unique<Model>(m_modelPath);
+//    m_shader ->ReadShader(m_shaderPath);
+    m_vao.Bind();
+    m_vbo = std::make_unique<VertexBuffer>(m_model->getData().data(),m_model->getDataSize());
+    m_ibo = std::make_unique<IndexBuffer>(m_model->getIndices().data(),(unsigned int)(m_model->getIndices().size()));
+    m_layout = std::make_shared<VertexBufferLayout>();
+    m_layout->Push(3, "float");//pos
+    m_layout->Push(3, "float");//color
+    m_layout->Push(2, "float");//uv
+    m_vao.AddBuffer(*m_vbo, *m_layout);
+    m_vao.UnBind();
+
+    
+}
+
+void GameObject::draw(const Renderer &renderer){
+    //behavior update@tode
+    renderer.Drawer(m_vao, *m_ibo, *m_shader);
+    
+}
